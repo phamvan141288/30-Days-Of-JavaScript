600e3ae9bab676b2d6e521e865f296ebbcd5d64d
phamvan141288 <van.pham@nextchain.kr>
2024-08-31 14:44:10 +0700

    learn 01 day string

--- 02_Day_Data_types/string_methods/accessing_character.js
+++ 02_Day_Data_types/string_methods/accessing_character.js
@@ -10,3 +10,10 @@ console.log(lastLetter) // t
 let lastIndex = string.length - 1
 console.log(lastIndex) // 9
 console.log(string[lastIndex]) // t
+
+/* nếu ghép các phép tính + - * /  thì đối với các ký tự
+chuỗi là số thì ( - / * ) sẽ quy đổi ký tự chuỗi thành số
+để thực hiện phép tính.
+Đặc biệt: thì phép + là phép nối chuỗi không thực hiện
+các phép toán tử số học như trên gọi là concatenation
+
