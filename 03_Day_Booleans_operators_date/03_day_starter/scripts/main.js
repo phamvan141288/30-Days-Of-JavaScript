7e3c30ca3d2bc848f437d80f8b79b068a515813f
phamvan141288 <van.pham@nextchain.kr>
2024-09-08 12:57:14 +0700

    learn about operate with condition ? and :

--- 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
+++ 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
@@ -5,3 +5,25 @@ let firstName1 = 'Asabeneh'
 let country1 = 'Finland'
 console.log(firstName ** lastName)
 console.log(firstName1 + ' ' + country1)
+
+
+let count = 0
+console.log(++count)        // 1
+console.log(count)          // 1
+
+let count1 = 0
+console.log(count1++)        // 0
+console.log(count1)          // 1
+
+
+// let isRaining = true
+// isRaining
+//   ? console.log('You need a rain coat.')
+//   : console.log('No need for a rain coat.')
+
+
+ let isRaining = false
+
+isRaining
+  ? console.log('You need a rain coat.')
+  : console.log('No need for a rain coat.')
