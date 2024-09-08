ad92da476ab1f362c5a22dc1a03f5e40d4d4349f
phamvan141288 <van.pham@nextchain.kr>
2024-09-08 12:19:10 +0700

    learning 03 day

--- 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
+++ 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
@@ -1,1 +1,7 @@
 // this is your main.js script
+let firstName = 310;
+let lastName = 22;
+let firstName1 = 'Asabeneh'
+let country1 = 'Finland'
+console.log(firstName ** lastName)
+console.log(firstName1 + ' ' + country1)
