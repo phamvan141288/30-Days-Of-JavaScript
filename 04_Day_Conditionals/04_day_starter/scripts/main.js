4c8f34c304187b1349683284d42885dd73ac570c
phamvan141288 <van.pham@nextchain.kr>
2024-09-17 09:06:26 +0700

    ngay hoc thu 4

--- 04_Day_Conditionals/04_day_starter/scripts/main.js
+++ 04_Day_Conditionals/04_day_starter/scripts/main.js
@@ -1,3 +1,60 @@
 // this is your main.js script
 
-alert('Open the browser console whenever you work on JavaScript')
+
+// let weather = 'rainy'
+// switch (weather) {
+//   case 'rainy':
+//     console.log('You need a rain coat.')
+//     break
+//   case 'cloudy':
+//     console.log('It might be cold, you need a jacket.')
+//     break
+//   case 'sunny':
+//     console.log('Go out freely.')
+//     break
+//   default:
+//     console.log(' No need for rain coat.')
+// }
+
+// let dayUserInput = prompt('What day is today ?')
+// let day = dayUserInput.toLowerCase()
+
+// switch (day) {
+//   case 'monday':
+//     console.log('Today is Monday')
+//     break
+//   case 'tuesday':
+//     console.log('Today is Tuesday')
+//     break
+//   case 'wednesday':
+//     console.log('Today is Wednesday')
+//     break
+//   case 'thursday':
+//     console.log('Today is Thursday')
+//     break
+//   case 'friday':
+//     console.log('Today is Friday')
+//     break
+//   case 'saturday':
+//     console.log('Today is Saturday')
+//     break
+//   case 'sunday':
+//     console.log('Today is Sunday')
+//     break
+//   default:
+//     console.log('It is not a week day.')
+// }
+
+let language = prompt("Enter your language: ")
+let word = language.toLocaleLowerCase();
+
+switch(word) {
+    case 'english':
+        console.log(`${language} is english`)
+        break
+    case 'korean':
+        console.log(`${language} is korean`)
+        break
+    default:
+        console.log('It is not a language')
+}
