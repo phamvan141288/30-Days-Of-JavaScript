8c88e0eed62756217ae11bd74500475db94625c9
phamvan141288 <van.pham@nextchain.kr>
2024-09-23 08:08:31 +0700

    bat dau bai hoc ngay thu 5 ve array

--- 05_Day_Arrays/05_day_starter/scripts/main.js
+++ 05_Day_Arrays/05_day_starter/scripts/main.js
@@ -1,3 +1,12 @@
-console.log(countries)
-alert('Open the browser console whenever you work on JavaScript')
-alert('Open the console and check if the countries has been loaded')
+// console.log(countries)
+// alert('Open the browser console whenever you work on JavaScript')
+// alert('Open the console and check if the countries has been loaded')
+
+const arr = [
+    'Asabeneh',
+    250,
+    true,
+    { country: 'Finland', city: 'Helsinki' },
+    { skills: ['HTML', 'CSS', 'JS', 'React', 'Python'] }
+] // arr containing different data types
+console.log(arr)
