5cbf9dc9ce978fd1a16c600c3a544e582f5c0fda
phamvan141288 <van.pham@nextchain.kr>
2024-09-24 09:38:31 +0700

    update ngay hoc thu 3 cuoi cung

--- 04_Day_Conditionals/04_day_starter/scripts/main.js
+++ 04_Day_Conditionals/04_day_starter/scripts/main.js
@@ -60,3 +60,21 @@
 // }
 
 // Lam bai tap nhe
+
+/**
+ 1. Write a code which  can give grades to students according to theirs scores:
+   - 80-100, A
+   - 70-89, B
+   - 60-69, C
+   - 50-59, D
+   - 0-49, F
+ */
+
+let score = prompt("Enter the score: ")
+ if (score >= 80 && score <= 100) {
+        console.log(`Your score ${score} is A grade`)
+    } else if (score >= 70 && score <= 89) {
+        console.log(`Your score ${score} is B grade`)
+    } else {
+        console.log(`Your score ${score} does not have grade`)
+    }
