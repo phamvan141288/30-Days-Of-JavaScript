d0cfda11738030808e32e902db81b76caf4f1895
phamvan141288 <van.pham@nextchain.kr>
2024-09-29 08:50:29 +0700

    ngay hoc thu 5 ve mang

--- 05_Day_Arrays/05_day_starter/scripts/main.js
+++ 05_Day_Arrays/05_day_starter/scripts/main.js
@@ -2,11 +2,30 @@
 // alert('Open the browser console whenever you work on JavaScript')
 // alert('Open the console and check if the countries has been loaded')
 
-const arr = [
-    'Asabeneh',
-    250,
-    true,
-    { country: 'Finland', city: 'Helsinki' },
-    { skills: ['HTML', 'CSS', 'JS', 'React', 'Python'] }
-] // arr containing different data types
-console.log(arr)
+// const arr = [
+//     'Asabeneh',
+//     250,
+//     true,
+//     { country: 'Finland', city: 'Helsinki' },
+//     { skills: ['HTML', 'CSS', 'JS', 'React', 'Python'] }
+// ] // arr containing different data types
+// console.log(arr)
+
+// Trong mảng [ {[]} ], có nghĩa trong mảng còn chưa được nhiều kiểu loại giá trị khác nhau bằng {} và có thể
+// khai báo thêm giá trị của mảng
+// Trong bài học này đã học được là Array có những hậu tố là split: (để chia); fill ( để thêm); lenght ( để tính chiều dài)
+
+
+// const numbers = [0, 3.14, 9.81, 37, 98.6, 100] 
+// console.log('Numbers are: ', numbers
+
+const ten = 'PhamKhacVan'
+const char = ten.split('')
+console.log(char.indexOf('P'));
+// console.log(ten.slice(0, 5));
+
+
+// let js = 'JavaScript'
+// const charsInJavaScript = js.split('')
+
+// console.log(charsInJavaScript) // ["J", "a", "v", "a", "S", "c", "r", "i", "p", "t"]
