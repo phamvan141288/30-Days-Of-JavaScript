8387222d400c874dc00691a347070d89c63b22ae
phamvan141288 <van.pham@nextchain.kr>
2024-10-14 08:31:41 +0700

    dang lam bai tap ngay thu 6, rat la hay

--- 06_Day_Loops/06_day_starter/scripts/main.js
+++ 06_Day_Loops/06_day_starter/scripts/main.js
@@ -50,22 +50,78 @@
 //        #######
 //    ```
 
-let m = [];
-let heights = 1;
-let space;
-for (i = 0; i <= heights; i++) {
-    space = ''.repeat(heights-i);
-    m += '#'.repeat(heights*i-1)
-    console.log(space + m)
+// let heights = 7;
+// for (i = 1; i <= heights; i++) {
+//     let space = ' '.repeat(heights-i);
+//     let m = '#'.repeat(2*i - 1);
+//     console.log(space + m);
+//     }
+
+
+
+
+// const height = 7;  // Chiều cao của cây thông
+
+// for (let i = 1; i <= height; i++) {
+//     let space = ' '.repeat(height-i);   // Tạo khoảng trắng để căn giữa
+//     let stars = '*'.repeat(2 * i - 1);    // Tạo các ngôi sao cho mỗi dòng
+//     console.log(space + stars);           // Hiển thị khoảng trắng + ngôi sao
+// }
+
+// 5. Use loop to print the following pattern:
+
+//    ```sh
+//    0 x 0 = 0
+//    1 x 1 = 1
+//    2 x 2 = 4
+//    3 x 3 = 9
+//    4 x 4 = 16
+//    5 x 5 = 25
+//    6 x 6 = 36
+//    7 x 7 = 49
+//    8 x 8 = 64
+//    9 x 9 = 81
+//    10 x 10 = 100
+//    ```
+
+// for ( let i = 0; i <= 10; i++) {
+//  console.log(`${i} * ${i} = ${i * i}`);
+// }
+
+// 6. Using loop print the following pattern
+
+//    ```sh
+//     i    i^2   i^3
+//     0    0     0
+//     1    1     1
+//     2    4     8
+//     3    9     27
+//     4    16    64
+//     5    25    125
+//     6    36    216
+//     7    49    343
+//     8    64    512
+//     9    81    729
+//     10   100   1000
+//    ```
+console.log('i i^2 i^3');
+for (let i =0 ; i <= 10; i++) {
+//     // if ( i % 2 === 0) {
+//     //     console.log(i);
+//     // }
+
+    // if ( i % 2 !== 0) {
+    //     console.log(i);
+    // }
+    if (i % i !== 0 || i / 1 !== i) {
+        console.log(i);
+    } else  {
+        
     }
-
-
-
-
-const height = 7;  // Chiều cao của cây thông
-
-for (let i = 1; i <= height; i++) {
-    let space = ' '.repeat(height-i);   // Tạo khoảng trắng để căn giữa
-    let stars = '*'.repeat(2 * i - 1);    // Tạo các ngôi sao cho mỗi dòng
-    console.log(space + stars);           // Hiển thị khoảng trắng + ngôi sao
+    
+    // console.log(`${i}  ${i**2} ${i**3}`);
 }
+
+// chia hết cho chính nó
+// chi hết cho 1
+// không thể chia cho số khác ngoài chính nó
