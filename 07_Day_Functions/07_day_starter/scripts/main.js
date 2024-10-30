ea1f05c50615bb629318dd64caa9f590cd1fc2ce
phamvan141288 <van.pham@nextchain.kr>
2024-10-30 08:33:41 +0700

    hoc ly thuyet day 7

--- 07_Day_Functions/07_day_starter/scripts/main.js
+++ 07_Day_Functions/07_day_starter/scripts/main.js
@@ -1,2 +1,45 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+// console.log(countries)
+// alert('Open the console and check if the countries has been loaded')
+// function square () {
+//     let m = 2
+//     let sq = m * m
+//     console.log(sq)
+// }
+// square();
+
+
+// function square() {
+//     let num = 2
+//     let sq = num * num
+//     return sq
+//   }
+//   console.log(square())
+
+//   function areaOfCircle(r) {
+//     let area = Math.PI * r * r
+//     return area
+//   }
+  
+//   console.log(areaOfCircle(1)) // should be called with one argument
+
+
+//   // function with two parameters
+// function functionName(parm1, parm2) {
+//     //code goes her
+//   }
+//   functionName(parm1, parm2) // during calling or invoking two arguments needed
+//   // Function without parameter doesn't take input, so lets make a function with parameters
+//   function sumTwoNumbers(numOne, numTwo) {
+//     let sum = numOne + numTwo
+//     console.log(sum)
+//   }
+//   sumTwoNumbers(10, 20) // calling functions
+//   // If a function doesn't return it doesn't store data, so it should return
+
+
+  function sumTwoNumbers(numOne, numTwo) {
+    let sum = numOne + numTwo;
+    console.log(sum);
+}
+
+sumTwoNumbers(10, 20); // Calls the function with arguments 10 and 20
