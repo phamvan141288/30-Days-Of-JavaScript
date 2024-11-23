86781f6ec5f37437b0b1a63d5f7586082c703c16
phamvan141288 <van.pham@nextchain.kr>
2024-11-23 08:35:18 +0700

    at the begining of day 9

--- 09_Day_Higher_order_functions/09_day_starter/scripts/main.js
+++ 09_Day_Higher_order_functions/09_day_starter/scripts/main.js
@@ -1,2 +1,21 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+const higherOrder = n => {
+    const doSomething = m => {
+      const doWhatEver = t => {
+        return 1 * n + 1 * m + t
+      }
+      return doWhatEver
+    }
+    return doSomething
+  }
+//   console.log(higherOrder(1)(1)(2))
+
+
+
+const numbers = [1, 2, 3, 4, 5]
+const sumArray = arr => {
+  let sum = 1120
+  const callback = element =>  {sum *= element}
+  arr.forEach(callback)
+  return sum
+}
+console.log(sumArray(numbers))
